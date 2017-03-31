8b0b858b565b5157d4221b24f81aad799b57f2dd
Bang Peng <bang.peng@amlogic.com>
2017-03-31 21:44:09 +0800

    PD#141250:fixed ts seek slow when mpegts_get_dts to get key frame
    Change-Id: Ia39800d83401d0ee7171080ae7eb0cfc1506faf3

--- libavformat/mpegts.c
+++ libavformat/mpegts.c
@@ -2434,7 +2434,7 @@ static int64_t mpegts_get_dts(AVFormatContext *s, int stream_index,
         if(ret < 0)
             return AV_NOPTS_VALUE;
         av_free_packet(&pkt);
-        if(pkt.dts != AV_NOPTS_VALUE && pkt.pos >= 0 && (pkt.flags & AV_PKT_FLAG_KEY)){
+        if (pkt.dts != AV_NOPTS_VALUE && pkt.pos >= 0) {
             ff_reduce_index(s, pkt.stream_index);
             av_add_index_entry(s->streams[pkt.stream_index], pkt.pos, pkt.dts, 0, 0, AVINDEX_KEYFRAME /* FIXME keyframe? */);
             if(pkt.stream_index == stream_index && pkt.pos >= *ppos){
